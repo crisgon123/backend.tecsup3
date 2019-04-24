0a9e73fb67cec6a580845e152b74df41e0026dd1
crisgon123 <crisgon123@gmail.com>
2019-04-24 16:12:09 -0500

    Diapos y Telefono.cs

---
+++ Semana 10/Dia 3/Telefono.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Relaciones
+{
+   public class Telefono
+   {
+      public int id { get; set; }
+      public int numeroTelefonico { get; set; }
+
+      public virtual Estudiante Estudiante { get; set; }
+   }
+}
